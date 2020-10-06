4ea99a12e8f941cec79bbd78f40285a64fa05458
Mark Brazee <m.brazee@comcast.net>
2020-10-06 08:58:19 -0600

    additional work on html css and js

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,31 @@
 <src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 
-// This is our API key. Add your own API key between the ""
+// OpenWeather API key  ""
 var APIKey = "87256196de89d20e22c222771dbc9601";
 
-// Here we are building the URL we need to query the database
+// OpenWeather URL to query the database
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
 
-// URL to query the database for the UV data
+// OpenWeather URL to query the database for the UV data
 var queryURLuv = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=" + APIKey;
 
 
 // Create an AJAX call
+
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
     console.log(response);
 
+    // create variables for city, wind, humidity, UV Index and temperature
+
     var cityDiv = $(".city").text(response.main.name);
     var windDiv = $(".wind").text(response.wind.speed);
     var humDiv = $(".humidity").text(respsone.main.humidity);
     var uvIndexDiv = $(".uvIndex").text( )
 
+    // convert Kelvin to Farenheit temperature 
     var kelvTemp = parseInt(response.main.temp)
     var farTemp = (kelvTemp - 273.5) * 1.80 + 32;
     var tempDiv = (".temp").text(farTemp)
